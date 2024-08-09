f6dff89ba0187051a879917c2a8f52e1b1003b06
Khaled Mahmoud <88454987+KhaledNegm22@users.noreply.github.com>
2024-08-09 17:35:00 +0300

    Add files via upload

---
+++ Is it a tree/Is it a Tree/IsTree/CheckTree.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IsATree
+{
+    public class CheckTree
+    {
+        //===============================================================================
+        //Your Code is Here:
+
+        public static bool IsTree(string[] vertices, List<KeyValuePair<string, string>> edges)
+        {
+
+            int vLen = vertices.Length;
+            int edgCount = edges.Count;
+
+            Dictionary<string, List<string>> adjs = new Dictionary<string, List<string>> { };
+            Dictionary<string, int> Dict = new Dictionary<string, int> { };
+
+            bool[] visited = new bool[vLen];
+            int i = 0;
+            while (i < vLen)
+            {
+                visited[i] = !true;
+                i++;
+            }
+            i = 0;
+            while (i < vLen)
+            {
+
+                adjs.Add(vertices[i], new List<string> { });
+
+                Dict.Add(vertices[i], i);
+                i++;
+            }
+            i = 0;
+            while (i < edgCount)
+            {
+                adjs[edges[i].Key].Add(edges[i].Value);
+                i++;
+            }
+            i = 0;
+            while (i < vLen)
+            {
+                if (visited[i] != true)
+                {
+                    if (Depth_1st_Search(vertices, adjs, visited, Dict,i)!=true)
+                    {
+                        return !true;
+                    }
+                }
+                i++;
+            }
+            return true;
+        }
+        public static bool Depth_1st_Search(string[] vertx, Dictionary<string, List<string>> adjs, bool[] visited, Dictionary<string, int> dict2, int inx)
+        {
+            visited[inx] = true;
+
+            foreach (var value in adjs[vertx[inx]])
+            {
+                if (visited[dict2[value]] == true || visited[dict2[value]] != true && Depth_1st_Search(vertx, adjs, visited, dict2, dict2[value]) != true)
+                { return !true; }
+            }
+
+            visited[inx] = !true;
+
+            return true;
+        }
+
+        //===============================================================================
+
+        //===============================================================================
+    }
+}
---
+++ [1] Currency Exchange/CurrencyExchange/CurrencyExchange/CurrencyExchange.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyExchange
+{
+    // *****************************************
+    // DON"T CHANGE CLASS NAME OR FUNCTION NAME
+    // *****************************************
+    public static class CurrencyExchange
+    {
+        //Your Code is Here:
+        //==================
+        /// <summary>
+        /// find the index of the USD Dollar $ with the smallest number of questions.
+        /// </summary>
+        /// <param name="N">Number of customers (N)</param>
+        /// <param name="M">Number of currencies (M)</param>
+        /// <param name="knows">N*M Matrix indicating whether customer i knows currency j or not</param>
+        /// <returns>index of US Dollar</returns>
+        public static int CheckUSD(int N, int M, bool[,] knows)
+        {
+            //knows[P,C]=true if person P knows currency C and knows[P,C]=false if person P doesn't know Currency C.
+            int j = 0;
+            int res = 0;
+            int inx=0;
+               for (int i = 0; i < M; i++)//rows
+               {
+                   if (knows[j, i] == true && res == 0)
+                   {
+                       res ++;
+                       inx = i;
+                   }
+                   else if (knows[j, i] == true && res == 1)
+                   {
+                    j++;
+                    res = 0;
+                    i = -1;
+                    inx = 0;
+                    continue;
+                    }
+                   if (i == M - 1 && res == 1)
+                   {
+                       return inx;
+                   }
+                   if (i == M - 1 && j < N - 1)
+                   {
+                       j++;
+                       res = 0;
+                       i = -1;
+                       inx = 0;
+                   }
+               }
+        
+            return -1;
+        }
+    }
+}
+
